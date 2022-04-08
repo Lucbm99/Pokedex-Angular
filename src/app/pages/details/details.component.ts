6c210808a123c022a9b028dc3d5b9253f2e53b19
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-04-07 21:37:43 -0300

    Fix - adição de imagens de erros + página detalhes do pokemon

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -13,23 +13,31 @@ export class DetailsComponent implements OnInit {
   private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
   private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  public pokemon: any;
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
+
   constructor(
     private _activatedRoute: ActivatedRoute,
     private _pokeApiService: PokeApiService
   ) { }
 
   ngOnInit(): void {
-    this.pokemon;
+    this.getPokemon;
   }
 
-  get pokemon() {
+  get getPokemon() {
     const id = this._activatedRoute.snapshot.params['id'];
     const pokemon = this._pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
     const name = this._pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
 
     return forkJoin([pokemon, name]).subscribe(
       res => {
-        console.log(res);
+        this.pokemon = res; 
+        this.isLoading = true;
+      },
+      error => {
+        this.apiError = true
       }
     )
 
--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -11,6 +11,8 @@ export class PokeListComponent implements OnInit {
   private setAllPokemons: any; 
   public getAllPokemons: any; 
 
+  public apiError: boolean = false
+
   constructor(
     private _pokeApiService: PokeApiService
   ) { }
@@ -20,6 +22,9 @@ export class PokeListComponent implements OnInit {
       res => {
         this.setAllPokemons = res.results;
         this.getAllPokemons = this.setAllPokemons;
+      },
+      error => {
+        this.apiError = true;
       }
     )
   }
